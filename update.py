5ca04499caf03e3032541d350db4f2e4b0640e31
GamerNoTitle <GamerNoTitle@outlook.com>
2023-07-21 14:55:15 +0800

    Added Some Pictures for my Hexo Blog

---
+++ update.py
@@ -0,0 +1,20 @@
+# update.py
+import json
+
+def update_version(package_file):
+    with open(package_file, 'r') as f:
+        data = json.load(f)
+
+    version = data.get('version', '1.0.0')
+    major, minor, patch = map(int, version.split('.'))
+    patch += 1
+    new_version = f"{major}.{minor}.{patch}"
+
+    data['version'] = new_version
+
+    with open(package_file, 'w') as f:
+        json.dump(data, f, indent=2)
+
+if __name__ == "__main__":
+    package_file = "package.json"
+    update_version(package_file)
