bf3b3e5436cd4140c5aaf990090cae134797b8bb
GamerNoTitle <GamerNoTitle@outlook.com>
2023-07-23 21:43:02 +0800

    Added Some Pictures for my Hexo Blog

--- Upload.bat
+++ Upload.bat
@@ -1,8 +1,4 @@
 @echo off
-setlocal
-
-set "packageJson=package.json"
-set "versionField=version"
 
 :upload
 echo Pulling changes from the remote repository...
@@ -11,20 +7,9 @@ git pull
 echo Checking for local changes...
 git status | find /i "nothing to commit, working tree clean" > nul
 if errorlevel 1 (
-  echo Local files have changed. Updating version...
-  
-  rem Call the Python script to update package.json
-  python update.py
-
   echo Adding and committing changes...
   git add -A
   git commit -m "Added Some Pictures for my Hexo Blog"
   git push -f
-
-  echo Publishing to npm...
-  npm publish --proxy http://127.0.0.1:10809
-) else (
-  echo No local changes found. Skipping version update.
 )
-
 goto upload
