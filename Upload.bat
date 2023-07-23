d2a48e612777f7cdacda56f8d3124f0556d02eac
GamerNoTitle <GamerNoTitle@outlook.com>
2023-07-23 21:44:06 +0800

    Added Some Pictures for my Hexo Blog

--- Upload.bat
+++ Upload.bat
@@ -1,12 +1,11 @@
 @echo off
 
 :upload
-echo Pulling changes from the remote repository...
-git pull
-
-echo Checking for local changes...
 git status | find /i "nothing to commit, working tree clean" > nul
 if errorlevel 1 (
+  echo Detected local changes!
+  echo Pulling changes from the remote repository...
+  git pull
   echo Adding and committing changes...
   git add -A
   git commit -m "Added Some Pictures for my Hexo Blog"
